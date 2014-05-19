aecee496513cf21649bdfb9c6a3354a046a45b08
Comyar <comyarzaheri@utexas.edu>
2014-05-19 15:40:23 -0500

    further defined CZWeatherRequest

--- CZWeatherKit/Request/CZWeatherRequest.h
+++ CZWeatherKit/Request/CZWeatherRequest.h
@@ -10,16 +10,73 @@
 #pragma mark - Imports
 
 @import Foundation;
+#import "CZWeatherService.h"
+
+#pragma mark - Forward Declarations
+
+@class CZWeatherData;
 
 
+#pragma mark - Constants
+
+/** 
+ */
+typedef NS_ENUM(u_int8t, CZWeatherRequestDetail) {
+    /** */
+    CZWeatherRequestNoDetail = 0,
+    /** */
+    CZWeatherRequestLightDetail,
+    /** */
+    CZWeatherRequestFullDetail
+};
+
+/**
+ */
+typedef void (^CZWeatherRequestCompletion) (CZWeatherData *weatherData, NSError *error);
+
+/**
+ */
+extern NSString * const CZWeatherRequestErrorDomain;
+
+ 
 #pragma mark - CZWeatherRequest Interface
 
 /**
  */
 @interface CZWeatherRequest : NSObject
 
+// -----
+// @name Configuring a Weather Request
+// -----
 
-//@property (nonatomic)
+/**
+ */
+- (void)completion:(CZWeatherRequestCompletion)completion;
 
+// -----
+// @name Using a Weather Request
+// -----
+
+/**
+ */
+- (void)start;
+
+// -----
+// @name Properties
+// -----
+
+#pragma mark - Properties
+
+/**
+ */
+@property (nonatomic)           id<CZWeatherService>    service;
+
+/**
+ */
+@property (nonatomic, readonly) CZWeatherRequestDetail  conditionsDetail;
+
+/**
+ */
+@property (nonatomic, readonly) CZWeatherRequestDetail  forecastDetail;
 
 @end
--- CZWeatherKit/Request/CZWeatherRequest.m
+++ CZWeatherKit/Request/CZWeatherRequest.m
@@ -6,8 +6,53 @@
 //
 //
 
+
+#pragma mark - Imports
+
 #import "CZWeatherRequest.h"
 
+
+#pragma mark - Constants
+
+NSString * const CZWeatherRequestErrorDomain = @"CZWeatherRequestErrorDomain";
+
+
+#pragma mark - CZWeatherRequest Class Extension
+
+@interface CZWeatherRequest ()
+
+//
+@property (nonatomic) BOOL                          hasStarted;
+
+//
+@property (nonatomic) CZWeatherRequestCompletion    completion;
+
+@end
+
+
+#pragma mark - CZWeatherRequest Implementation
+
 @implementation CZWeatherRequest
+
+- (void)completion:(CZWeatherRequestCompletion)completion
+{
+    if (!self.hasStarted) {
+        self.completion = completion;
+    }
+}
+
+- (void)start
+{
+    self.hasStarted = YES;
+    
+    // Get URL from service
+    
+    // Send request
+    
+    // Parse data using service
+    
+    // Run completion block
+}
+
 
 @end
