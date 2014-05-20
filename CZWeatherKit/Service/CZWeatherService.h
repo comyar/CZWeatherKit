2da8b06456ad162fe5c2d742284283abaafd6d5d
Comyar <comyarzaheri@utexas.edu>
2014-05-19 22:31:48 -0500

    cleaned up and further defined CZWeatherService protocol

--- CZWeatherKit/Service/CZWeatherService.h
+++ CZWeatherKit/Service/CZWeatherService.h
@@ -21,19 +21,33 @@
 #pragma mark - Protocol
 
 /**
+ The CZWeatherService protocol outlines an interface for objects to implement in order to act
+ as services for weather requests. Objects adopting the CZWeatherService protocol provide URLs
+ for specific weather APIs and parse downloaded data.
  */
 @protocol CZWeatherService <NSObject>
 
-/**
- */
-+ (instancetype)service;
+@required
+
+// -----
+// @name Creating a Weather Service
+// -----
+
+#pragma mark Creating a Weather Service
 
 /**
+ Returns the appropriate URL for the given request.
+ @param request Weather requests asking for the URL.
+ @return        URL for the given request.
  */
 - (NSURL *)urlForRequest:(CZWeatherRequest *)request;
 
 /**
+ Parses the response data from the given request.
+ @param data    Response data from a weather request.
+ @param request Weather request that retrieved the response data.
+ @return        Weather data instance from the parsed response data.
  */
-- (CZWeatherData *)weatherDataForResponseData:(NSData *)data;
+- (CZWeatherData *)weatherDataForResponseData:(NSData *)data request:(CZWeatherRequest *)request;
 
 @end
