ba9d5b5a2cf449d3561eb1f9bd094b2eaaf969f7
Comyar <comyarzaheri@utexas.edu>
2014-05-24 23:50:21 -0500

    added open weather map tests file

---
+++ CZWeatherKit Tests/CZWeatherKitTests/CZOpenWeatherMapServiceTests.m
@@ -0,0 +1,21 @@
+//
+//  CZOpenWeatherMapServiceTests.m
+//  CZWeatherKit Tests
+//
+//  Created by Comyar Zaheri on 5/24/14.
+//  Copyright (c) 2014 Comyar Zaheri. All rights reserved.
+//
+
+
+#pragma mark - CZOpenWeatherMapServiceTests Interface
+
+@interface CZOpenWeatherMapServiceTests : CZWeatherServiceTestCase
+
+@end
+
+
+#pragma mark - CZOpenWeatherMapServiceTests Implementation
+
+@implementation CZOpenWeatherMapServiceTests
+
+@end
