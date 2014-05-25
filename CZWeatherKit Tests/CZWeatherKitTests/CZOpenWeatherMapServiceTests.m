93232cbb015414cdd5a2426d60391063844cf5cf
Comyar <comyarzaheri@utexas.edu>
2014-05-25 01:09:01 -0500

    updated gitignore

--- CZWeatherKit Tests/CZWeatherKitTests/CZOpenWeatherMapServiceTests.m
+++ CZWeatherKit Tests/CZWeatherKitTests/CZOpenWeatherMapServiceTests.m
@@ -7,7 +7,7 @@
 //
 
 #import "CZWeatherServiceTestCase.h"
-
+#import "CZOpenWeatherMapService.h"
 
 #pragma mark - CZOpenWeatherMapServiceTests Interface
 
@@ -19,5 +19,16 @@
 #pragma mark - CZOpenWeatherMapServiceTests Implementation
 
 @implementation CZOpenWeatherMapServiceTests
+
+- (void)setUp
+{
+    self.service = [CZOpenWeatherMapService serviceWithKey:[self loadFile:@"" extension:@"txt"]];
+}
+
+- (NSString *)loadFile:(NSString *)filename extension:(NSString *)extension
+{
+    NSString *path      = [[NSBundle bundleForClass:[self class]]pathForResource:filename ofType:extension];
+    return [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+}
 
 @end
