2fab10c93a94f4f0a2f5153c692ac8021ad63b73
Comyar <comyarzaheri@utexas.edu>
2014-05-27 19:38:51 -0500

    updated weather location tests

--- CZWeatherKit Tests/CZWeatherKitTests/CZWeatherLocationTestCase.m
+++ CZWeatherKit Tests/CZWeatherKitTests/CZWeatherLocationTestCase.m
@@ -1,59 +1,103 @@
 //
-//  CZWeatherLocationTestCase.m
-//  CZWeatherKit Tests
+// CZWeatherLocationTestCase.m, Created by Eli Perkins
+// Copyright (c) 2014, Comyar Zaheri, http://comyar.io
+// All rights reserved.
 //
-//  Created by Eli Perkins on 5/27/14.
-//  Copyright (c) 2014 Comyar Zaheri. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
 //
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#import <XCTest/XCTest.h>
-#import "CZWeatherKit.h"
+
+#pragma mark - Imports
+@import XCTest;
+#import "CZWeatherLocation.h"
+
+
+#pragma mark - CZWeatherLocationTestCase Interface
 
 @interface CZWeatherLocationTestCase : XCTestCase
 
 @end
 
+
+#pragma mark - CZWeatherLocationTestCase Implementation
+
 @implementation CZWeatherLocationTestCase
 
-- (void)testCountryCityName
+- (void)test_locationWithCityCountry
 {
-    CZWeatherLocation *location = [CZWeatherLocation locationWithCountryCityName:@"England/London"];
-    XCTAssertEqual(location.countryCityName, @"England/London", @"Country city names not equal");
+    CZWeatherLocation *location = [CZWeatherLocation locationWithCity:@"London" country:@"England"];
+    XCTAssertEqualObjects(location.locationData[CZWeatherLocationCityName], @"London", @"City name not equal");
+    XCTAssertEqualObjects(location.locationData[CZWeatherLocationCountryName], @"England", @"Country name not equal");
+    XCTAssertEqual(location.locationType, CZWeatherLocationCityCountryType, @"Location type is not correct");
 }
 
-- (void)testStateCityName
+- (void)test_locationWithCityState
 {
-    CZWeatherLocation *location = [CZWeatherLocation locationWithStateCityName:@"TX/Austin"];
-    XCTAssertEqual(location.stateCityName, @"TX/Austin", @"State city names not equal");
+    CZWeatherLocation *location = [CZWeatherLocation locationWithCity:@"Austin" state:@"TX"];
+    XCTAssertEqualObjects(location.locationData[CZWeatherLocationCityName], @"Austin", @"City name not equal");
+    XCTAssertEqualObjects(location.locationData[CZWeatherLocationStateName], @"TX", @"State name not equal");
+    XCTAssertEqual(location.locationType, CZWeatherLocationCityStateType, @"Location type is not correct");
 }
 
-- (void)testZipcode
+- (void)test_locationWithZipcode
 {
     CZWeatherLocation *location = [CZWeatherLocation locationWithZipcode:@"02110"];
-    XCTAssertEqual(location.zipcode, @"02110", @"Zipcodes not equal");
+    XCTAssertEqual(location.locationData[CZWeatherLocationZipcodeName], @"02110", @"Zipcode not equal");
+    XCTAssertEqual(location.locationType, CZWeatherLocationZipcodeType, @"Location type is not correct");
 }
 
-- (void)testAutoIP
+- (void)test_locationWithAutoIP
 {
     CZWeatherLocation *location = [CZWeatherLocation locationWithAutoIP];
-    XCTAssertTrue(location.autoIP, @"Auto IP not set to true");
+    XCTAssertEqual(location.locationType, CZWeatherLocationAutoIPType, @"Location type is not correct");
 }
 
-- (void)testLocationCoordinate
+- (void)test_locationWithCLLocationCoordinate2D
 {
-    CZWeatherLocation *location = [CZWeatherLocation locationWithLatitude:-45.6 longitude:13.2];
+    const double accuracy               = 0.001;
+    const CLLocationDegrees latitude    = -45.6;
+    const CLLocationDegrees longitude   = 13.2;
+
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(latitude, longitude);
+    CZWeatherLocation *location = [CZWeatherLocation locationWithCLLocationCoordinate2D:coordinate];
     
-    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(-45.6, 13.2);
+    CGPoint coordinatePoint = [location.locationData[CZWeatherLocationCoordinateName] CGPointValue];
+    XCTAssertEqualWithAccuracy(coordinatePoint.x, latitude, accuracy, @"Location latitude not equal");
+    XCTAssertEqualWithAccuracy(coordinatePoint.y, longitude, accuracy, @"Location longitude not equal");
+    XCTAssertEqual(location.locationType, CZWeatherLocationCoordinateType, @"Location type is not correct");
+}
+
+- (void)test_locationWithCLLocation
+{
+    const double accuracy   = 0.001;
+    const CLLocationDegrees latitude  = -45.6;
+    const CLLocationDegrees longitude = 13.2;
     
-    double accuracy = 0.001;
+    CLLocation *location = [[CLLocation alloc]initWithLatitude:latitude longitude:longitude];
+    CZWeatherLocation *weatherLocation = [CZWeatherLocation locationWithCLLocation:location];
     
-    XCTAssertEqualWithAccuracy(location.locationCoordinate.latitude, coordinate.latitude, accuracy, @"Location latitudes not equal");
-    XCTAssertEqualWithAccuracy(location.locationCoordinate.longitude, coordinate.longitude, accuracy, @"Location longitudes not equal");
-    
-    location = [CZWeatherLocation locationWithCoordinatePoint:CLLocationCoordinate2DMake(-45.6, 13.2)];
-    
-    XCTAssertEqualWithAccuracy(location.locationCoordinate.latitude, coordinate.latitude, accuracy, @"Location latitudes not equal");
-    XCTAssertEqualWithAccuracy(location.locationCoordinate.longitude, coordinate.longitude, accuracy, @"Location longitudes not equal");
+    CGPoint coordinatePoint = [weatherLocation.locationData[CZWeatherLocationCoordinateName] CGPointValue];
+    XCTAssertEqualWithAccuracy(coordinatePoint.x, latitude, accuracy, @"Location latitude not equal");
+    XCTAssertEqualWithAccuracy(coordinatePoint.y, longitude, accuracy, @"Location longitude not equal");
+    XCTAssertEqual(weatherLocation.locationType, CZWeatherLocationCoordinateType, @"Location type is not correct");
 }
 
 @end
